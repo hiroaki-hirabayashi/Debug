da49d781b4e602d883eafde103838f881f15be5e
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-02-16 02:55:46 +0900

    雛形作成

--- app/src/main/java/com/example/debug/MainActivity.kt
+++ app/src/main/java/com/example/debug/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.debug
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logging()
+    }
+
+    fun logging() {
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
 }
